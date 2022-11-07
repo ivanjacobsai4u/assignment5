8087b3b598a5a473b5f645c6684f1c89cc630e3f
ivanjacobsai4u <53968200+ivanjacobsai4u@users.noreply.github.com>
2022-11-07 08:24:44 +0100

    Test if any attributes have a correlation above 70% between them. Test fails

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
 import os
+import numpy as np
 
 df_train = pd.read_csv("data/train.csv", sep=',')
 df_test = pd.read_csv("data/test.csv")
@@ -55,3 +56,9 @@ def test_extreme_values_numerical_columns():
     )
 
     assert extreme_values == 0
+
+def test_features_high_correlation():
+    x_train = df_train.loc[:, df_train.columns != "SalePrice"]
+    x_corr_matrix = x_train.corr().abs()
+    upper_triangle = x_corr_matrix.where(np.triu(np.ones(x_corr_matrix.shape), k=1).astype(np.bool))
+    assert len( [column for column in upper_triangle.columns if any(upper_triangle[column] > 0.70)])==0
